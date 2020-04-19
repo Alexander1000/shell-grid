7a8f40ffc06c6a87126c453aba2d4c57e5f7b4c2
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-19 12:24:56 +0300

    lazy allocation rows

--- src/grid.cpp
+++ src/grid.cpp
@@ -12,6 +12,27 @@ namespace ShellGrid
 
     void Grid::Set(int row, int column, class ShellGrid::Cell* cell)
     {
+        int curRow = 0;
+        std::list<RowData*>::iterator itRows;
+        for (itRows = this->data.begin(); itRows != this->data.end(); ++itRows) {
+            if (curRow == row) {
+                break;
+            }
+            curRow++;
+        }
 
+        RowData* rowData = nullptr;
+
+        if (itRows == this->data.end()) {
+            // not found row, then generate new
+            for (int i = curRow; i < row; ++i) {
+                rowData = new RowData;
+                this->data.push_back(rowData);
+            }
+        } else {
+            rowData = *itRows;
+        }
+
+        // rowData - current need row for insert
     }
 }
