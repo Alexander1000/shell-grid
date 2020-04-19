6bae2186a4161d1f121c7a704fd89962aefe874b
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-19 11:23:15 +0300

    update cell

--- include/shell-grid/cell.h
+++ include/shell-grid/cell.h
@@ -1,10 +1,14 @@
 #ifndef H_SHELL_GRID_CELL_INCLUDED
 #define H_SHELL_GRID_CELL_INCLUDED
 
+#include <shell-grid/cell-type.h>
+
 namespace ShellGrid
 {
     class Cell
     {
+    public:
+        virtual CellType GetType() = 0;
     };
 }
 
