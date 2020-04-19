ffbded1761629d40b9a86134d7770b4deeb3613d
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-19 11:42:24 +0300

    make tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,3 +1,18 @@
+#include <cpp-unit-test.h>
+
+CppUnitTest::TestCase* testGrid_SimpleData_Positive()
+{
+    CppUnitTest::TestCase* t = new CppUnitTest::TestCase("001-simple-test");
+    t->printTitle();
+    t->finish();
+    return t;
+}
+
 int main() {
+    CppUnitTest::TestSuite testSuite;
+
+    testSuite.addTestCase(testGrid_SimpleData_Positive());
+
+    testSuite.printTotal();
     return 0;
 }
