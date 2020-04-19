52914e403288e259cf20d1c376b7dbba16c9ce83
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-19 11:27:21 +0300

    make interface

--- include/shell-grid/grid.h
+++ include/shell-grid/grid.h
@@ -1,10 +1,20 @@
 #ifndef H_SHELL_GRID_GRID_INCLUDED
 #define H_SHELL_GRID_GRID_INCLUDED
 
+#include <list>
+#include <shell-grid/cell.h>
+
+typedef std::list<*Cell> RowData;
+
 namespace ShellGrid
 {
     class Grid
     {
+    public:
+        Grid();
+        Grid(int rows, int columns);
+    private:
+         std::list<*RowData> data;
     };
 }
 
