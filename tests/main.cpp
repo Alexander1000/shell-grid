870a78ebe89120608f8543b90a1a90a57946bc06
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-25 21:23:11 +0300

    add test-case

--- tests/main.cpp
+++ tests/main.cpp
@@ -22,10 +22,35 @@ CppUnitTest::TestCase* testGrid_SimpleData_Positive()
     return t;
 }
 
+CppUnitTest::TestCase* testGrid_SimpleData2_Positive()
+{
+    CppUnitTest::TestCase* t = new CppUnitTest::TestCase("002-simple-test");
+    t->printTitle();
+
+    int nRows = 6, nCols = 8;
+
+    ShellGrid::Grid grid(nRows, nCols);
+
+    for (int i = 0; i < nRows; ++i) {
+        for (int j = 0; j < nCols; ++j) {
+            if (i % 2 == 0 || j % 2 == 0) {
+                grid.Set(i, j, new ShellGrid::CellNumeric(i * nCols + j));
+            }
+        }
+    }
+
+    grid.Output();
+
+    t->finish();
+    return t;
+}
+
 int main() {
     CppUnitTest::TestSuite testSuite;
 
     testSuite.addTestCase(testGrid_SimpleData_Positive());
+
+    testSuite.addTestCase(testGrid_SimpleData2_Positive());
 
     testSuite.printTotal();
     return 0;
