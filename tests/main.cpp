a597297c227857440ce58fd2c1e92559144d52f8
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-05-02 10:13:21 +0300

    update

--- tests/main.cpp
+++ tests/main.cpp
@@ -106,6 +106,35 @@ CppUnitTest::TestCase* testGrid_EmptyGrid_Positive()
     return t;
 }
 
+CppUnitTest::TestCase* testGrid_GridWithString_Positive()
+{
+    CppUnitTest::TestCase* t = new CppUnitTest::TestCase("006-grid-with-string-test");
+    t->printTitle();
+
+    int nRows = 4, nCols = 4;
+
+    ShellGrid::Grid grid(nRows, nCols);
+
+    for (int i = 1; i < nRows; ++i) {
+        for (int j = 1; j < nCols; ++j) {
+            grid.Set(i, j, new ShellGrid::CellNumeric((10 - i) * nCols + (i * j)));
+        }
+    }
+
+    grid.Set(0, 1, new ShellGrid::CellString("Column A"));
+    grid.Set(0, 2, new ShellGrid::CellString("Column B"));
+    grid.Set(0, 3, new ShellGrid::CellString("Column C"));
+
+    grid.Set(1, 0, new ShellGrid::CellString("Row I"));
+    grid.Set(2, 0, new ShellGrid::CellString("Row II"));
+    grid.Set(3, 0, new ShellGrid::CellString("Row III"));
+
+    grid.Output();
+
+    t->finish();
+    return t;
+}
+
 int main() {
     CppUnitTest::TestSuite testSuite;
 
@@ -118,6 +147,8 @@ int main() {
     testSuite.addTestCase(testGrid_SingleItem_Positive());
 
     testSuite.addTestCase(testGrid_EmptyGrid_Positive());
+
+    testSuite.addTestCase(testGrid_GridWithString_Positive());
 
     testSuite.printTotal();
     return 0;
